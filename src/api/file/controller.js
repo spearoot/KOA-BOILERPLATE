87825837624111ef98a6d9bf9752d2a0c418aa77
lee <spearoot1@gmail.com>
2023-01-17 14:05:30 +0900

    db연결성공, 파일관리 성공

--- src/api/file/controller.js
+++ src/api/file/controller.js
@@ -1,5 +1,29 @@
+const { create,show } = require('./query');
+const fs = require('fs');
 /** 파일 업로드 */
-exports.upload = ctx => {
-    let file = ctx.requests.file;
-    ctx.body = file;
+exports.upload = async (ctx) => {
+    let file = ctx.request.file;
+    
+    let { affectedRows, insertId } = await create(file.originalname, file.path, file.size);
+
+    if (affectedRows > 0) {
+        ctx.body = { result : "ok", id: insertId}
+    } else {
+        ctx.body = { result: 'fail', }
+    }
+}
+
+exports.download = async ctx => {
+    let { id } = ctx.params;
+
+    let item = await show(id);
+
+    if(item == null) {
+        ctx.body = {result: "fail"};
+        return;
+    }
+
+    ctx.response.set("content-disposition", `attachment; filename=${item.original_name}`);
+    ctx.statusCode = 200;
+    ctx.body = fs.createReadStream(item.file_path);
 }
--- src/data/index.js
+++ src/data/index.js
@@ -6,6 +6,7 @@ exports.connection = mysql.createPool(
         host:process.env.DB_HOST,
         user: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         waitForConnections: true,
         connectionLimit: 10,
         queueLimit: 0
