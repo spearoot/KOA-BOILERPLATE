b3e4d5c0d4dcb18df1f729bc8e0545dac6430751
lee <spearoot1@gmail.com>
2023-01-17 14:51:47 +0900

    암호화 적용

--- src/api/user/query.js
+++ src/api/user/query.js
@@ -12,3 +12,8 @@ exports.register = async (email, password, name) => {
     VALUES (?,?,?)`;
     return await pool(query, [email, password, name]);
 }
+exports.login = async (email, password) => {
+    const query = `SELECT * FROM user WHERE
+    email = ? AND password = ?`;
+    return (result.length < 0) ? null : result[0];
+}
